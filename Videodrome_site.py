ecfeab7b2dd661e87bf395d4f0704d8b352749c4
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-16 19:48:52 +0100

    Update Videodrome_site.py

--- Videodrome_site.py
+++ Videodrome_site.py
@@ -12,7 +12,7 @@ def load_and_prepare_data():
     # Chemin vers le fichier CSV (à remplacer par votre chemin de fichier)
     data_path = "https://github.com/Fernand-Naudin/Videodrome/tree/main/final_merged_imdb_akas_2023-11-26_16h02m36s.csv"
     # uploaded_file = st.file_uploader("https://github.com/Fernand-Naudin/Videodrome/tree/main/final_merged_imdb_akas_2023-11-26_16h02m36s.csv")
-    st.write(data_path)
+    data_path = st.write(data_path)
     
     # Chargement des données
     df = pd.read_csv(data_path, sep=",", low_memory=False)
