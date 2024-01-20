c1ff0503b1251a950f1580431d069f64ce713750
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 20:18:22 +0100

    Update Requete.py

--- Requete.py
+++ Requete.py
@@ -1,16 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-# Widget pour téléverser un fichier
-uploaded_file = st.file_uploader("Choisissez un fichier CSV", type="csv")
+@st.cache  # Utiliser le cache pour éviter de recharger les données à chaque interaction
+def load_data(url):
+    # Utiliser pandas pour lire le fichier CSV directement depuis l'URL
+    df = pd.read_csv(url)
+    return df
 
-if uploaded_file is not None:
-    # Lire le fichier CSV
-    df = pd.read_csv(uploaded_file, sep = ',')
-    
-    # Afficher le DataFrame dans l'application
-    st.write("Aperçu des données :")
-    st.write(df.head())  # Affiche les premières lignes du DataFrame
+# URL du fichier CSV sur GitHub (s'assurer que c'est l'URL du fichier brut/raw)
+csv_url = 'https://raw.githubusercontent.com/user/repo/branch/filename.csv'
+
+# Charger les données
+df = load_data(csv_url)
+
+# Afficher le DataFrame dans l'application
+st.write("Aperçu des données :")
+st.dataframe(df)
 
 
 
