52b78ab33e4052fe2823ee8d6b972d0c75dc4889
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 20:12:02 +0100

    Update Requete.py

--- Requete.py
+++ Requete.py
@@ -6,7 +6,7 @@ uploaded_file = st.file_uploader("Choisissez un fichier CSV", type="csv")
 
 if uploaded_file is not None:
     # Lire le fichier CSV
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(uploaded_file, sep = ',')
     
     # Afficher le DataFrame dans l'application
     st.write("Aperçu des données :")
