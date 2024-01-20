fd022f51d2443e8dcd7b7583526a202a8fe1c135
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 17:31:54 +0100

    Update Videodrome_site.py

--- Videodrome_site.py
+++ Videodrome_site.py
@@ -15,7 +15,7 @@ def load_and_prepare_data():
     # uploaded_file = st.file_uploader("https://github.com/Fernand-Naudin/Videodrome/tree/main/final_merged_imdb_akas_2023-11-26_16h02m36s.csv")
     
     # Chargement des données
-    df = pd.read_csv("final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv", sep=",", on_bad_lines='skip')
+    df = pd.read_csv(data_path, sep=",", on_bad_lines='skip')
     df.columns
     
     # Utilisation de la colonne 'genres' pour la similarité
