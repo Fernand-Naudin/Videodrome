a13e70ef322bc360a61c2da0088d9a717a3c193d
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 14:35:54 +0100

    Update Videodrome_site.py

--- Videodrome_site.py
+++ Videodrome_site.py
@@ -15,7 +15,7 @@ def load_and_prepare_data():
     
     # Chargement des données
     try:
-        df = pd.read_csv("https://github.com/Fernand-Naudin/Videodrome/tree/main/final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv", sep=",", low_memory=False, error_bad_lines=False, warn_bad_lines=True, quoting=csv.QUOTE_NONE)
+        df = pd.read_csv("https://github.com/Fernand-Naudin/Videodrome/tree/main/final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv", sep=",", low_memory=False, error_bad_lines=False, warn_bad_lines=True)
     except pd.errors.EmptyDataError:
         st.error("Le fichier CSV est vide ou n'existe pas à l'emplacement spécifié.")
     except Exception as e:
