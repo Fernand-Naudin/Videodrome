cbd44f33ae137a1b7244bd425c330d123db2db7b
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 22:52:40 +0100

    Update Videodrome_site.py

--- Videodrome_site.py
+++ Videodrome_site.py
@@ -18,7 +18,7 @@ def load_data(url):
 @st.cache_resource
 def load_and_prepare_data():
     # URL du fichier CSV sur GitHub (s'assurer que c'est l'URL du fichier brut/raw)
-    csv_url = 'https://raw.githubusercontent.com/Fernand-Naudin/Videodrome/main/final_merged_imdb_akas_filtered_4000.csv'
+    csv_url = 'https://raw.githubusercontent.com/Fernand-Naudin/Videodrome/main/final_merged_imdb_akas_filtered_4500.csv'
     
     # Charger les données
     df = load_data(csv_url)
