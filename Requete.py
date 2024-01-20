465692a4c63d3dcdbbdd03d5d9946786fd8e17aa
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 20:19:02 +0100

    Update Requete.py

--- Requete.py
+++ Requete.py
@@ -8,7 +8,7 @@ def load_data(url):
     return df
 
 # URL du fichier CSV sur GitHub (s'assurer que c'est l'URL du fichier brut/raw)
-csv_url = 'https://raw.githubusercontent.com/user/repo/branch/filename.csv'
+csv_url = 'https://raw.githubusercontent.com/Fernand-Naudin/Videodrome/main/final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv'
 
 # Charger les données
 df = load_data(csv_url)
