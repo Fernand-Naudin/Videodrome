28004c5c8040e4f3d3c2f7a10ea625159ea9584e
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 20:22:14 +0100

    Update Requete.py

--- Requete.py
+++ Requete.py
@@ -1,39 +1,20 @@
-# import streamlit as st
-# import pandas as pd
-
-# @st.cache  # Utiliser le cache pour éviter de recharger les données à chaque interaction
-# def load_data(url):
-#     # Utiliser pandas pour lire le fichier CSV directement depuis l'URL
-#     df = pd.read_csv(url)
-#     return df
-
-# # URL du fichier CSV sur GitHub (s'assurer que c'est l'URL du fichier brut/raw)
-# csv_url = 'https://raw.githubusercontent.com/Fernand-Naudin/Videodrome/main/final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv'
-
-# # Charger les données
-# df = load_data(csv_url)
-
-# # Afficher le DataFrame dans l'application
-# st.write("Aperçu des données :")
-# st.dataframe(df)
-
-
+import streamlit as st
 import pandas as pd
-import requests
-from io import StringIO
 
-# URL du fichier CSV sur GitHub
+@st.cache  # Utiliser le cache pour éviter de recharger les données à chaque interaction
+def load_data(url):
+    # Utiliser pandas pour lire le fichier CSV directement depuis l'URL
+    df = pd.read_csv(url)
+    return df
+
+# URL du fichier CSV sur GitHub (s'assurer que c'est l'URL du fichier brut/raw)
 csv_url = 'https://raw.githubusercontent.com/Fernand-Naudin/Videodrome/main/final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv'
 
-# Envoyer une requête HTTP GET pour obtenir le contenu du fichier CSV
-response = requests.get(csv_url)
-response.raise_for_status()  # Vérifier que la requête a réussi
+# Charger les données
+df = load_data(csv_url)
 
-# Convertir le contenu en texte en un objet fichier utilisable par Pandas
-csv_raw = StringIO(response.text)
+# Afficher le DataFrame dans l'application
+st.write("Aperçu des données :")
+st.dataframe(df)
 
-# Utiliser pandas pour lire le fichier CSV
-df = pd.read_csv(csv_raw)
 
-# Afficher les premières lignes du DataFrame pour vérifier
-print(df.head())
