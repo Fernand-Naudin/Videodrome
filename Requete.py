5d1706182ea7d3113a9a66f27559aa5608196218
Fernand-Naudin <144100201+Fernand-Naudin@users.noreply.github.com>
2024-01-20 20:09:35 +0100

    Update Requete.py

--- Requete.py
+++ Requete.py
@@ -6,18 +6,11 @@ from io import StringIO
 
 @st.cache  # Utiliser le cache pour éviter de recharger les données à chaque interaction
 def load_data(url):
-    # Envoyer une requête GET pour obtenir le contenu du fichier CSV
-    response = requests.get(url)
-    response.raise_for_status()  # Vérifier que la requête a réussi
-
-    # Convertir le contenu en texte en un objet fichier utilisable par Pandas
-    csv_raw = StringIO(response.text)
-
-    # Utiliser pandas pour lire le fichier CSV
-    df = pd.read_csv(csv_raw, sep = ',')
+    # Utiliser pandas pour lire le fichier CSV directement depuis l'URL
+    df = pd.read_csv(url, sep = ',')
     return df
 
-# URL du fichier CSV sur GitHub
+# URL du fichier CSV sur GitHub (s'assurer que c'est l'URL du fichier brut/raw)
 csv_url = 'https://raw.githubusercontent.com/Fernand-Naudin/Videodrome/main/final_merged_imdb_akas_2023-11-26_16h02m36s_m.csv'
 
 # Charger les données
